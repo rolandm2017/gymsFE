7a531d9c5f5fe46e8a84cac22fe4fc9c80bb2146
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-08-02 14:50:50 -0700

    use poppins

---
+++ src/components/ApartmentCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ApartmentCard";
+
+function ApartmentCard() {
+    return (
+        <div>
+            <h1>Hi</h1>
+            <h2 className="poppins">Poppins</h2>
+        </div>
+    )
+}
+
+export default ApartmentCard;
