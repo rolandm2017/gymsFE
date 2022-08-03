8f95705fdc9754c0cd09349fcc195d74013eeb79
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-08-03 14:06:40 -0700

    button done

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Button.scss";
+
+function Button() {
+    return (
+        <div>
+            <div className="buttonShared">
+                <p>Button</p>
+            </div>
+        </div>
+    );
+}
