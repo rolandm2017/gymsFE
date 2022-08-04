4f7b6b580f10a375cab04e3dae4c04853ac6dd22
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-08-04 15:14:48 -0700

    closer to finishing search bar draft

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -6,15 +6,17 @@ import "./SearchBar.scss";
 
 const SearchBar: React.FC<{}> = () => {
     return (
-        <div className="searchBarContainer w-full h-24 px-6 flex items-center debug1">
-            <div className="flex">
+        <div className="searchBarContainer w-full h-24 px-6 flex items-center">
+            <div className="w-full flex justify-between">
                 <div className="inputContainerLeft flex debug4">
                     <Input type="text" placeholder={"Address"} />
                     <Input type="text" placeholder={"City"} />
                 </div>
-                <div className="inputContainerRight flex debug5">
-                    <Input type="text" placeholder={"Min Distance"} />
-                    <Input type="text" placeholder={"Max Distance"} />
+                <div className="inputContainerRight w-full flex justify-end">
+                    <div className=" pr-8 flex debug5">
+                        <Input type="text" placeholder={"Min Distance"} />
+                        <Input type="text" placeholder={"Max Distance"} />
+                    </div>
                     <Button type="Opaque" text="Search" />
                 </div>
             </div>
