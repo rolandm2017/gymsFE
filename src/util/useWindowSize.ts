cdff8cd90f41476c15b167538f67aab4d0718ee2
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-08-08 21:15:57 -0700

    remove debug classes

--- src/util/useWindowSize.ts
+++ src/util/useWindowSize.ts
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 function useWindowSize() {
     const [size, setSize] = useState([0, 0]);
