f4fb58ecb9780bbf9f90c8ec2bc3042b8a39178e
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-12-27 19:03:47 -0800

    _ => -

--- src/api/queries/AdminQueries.ts
+++ src/api/queries/AdminQueries.ts
@@ -9,13 +9,13 @@ const headers = {
 };
 
 // this.router.get("/batches/all", authorize([Role.Admin]), this.getAllBatchNumbers.bind(this));
-//this.router.get("/task_queue/all", authorize([Role.Admin]), this.getAllTasks.bind(this));
-//this.router.get("/housing/by_location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
-// /task_queue/markers_by_batch_num
-//this.router.get("/housing/by_city_id_and_batch_num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
+//this.router.get("/task-queue/all", authorize([Role.Admin]), this.getAllTasks.bind(this));
+//this.router.get("/housing/by-location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
+// /task-queue/markers-by-batch-num
+//this.router.get("/housing/by-city-id-and-batch-num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
 //// user stuff
 //this.router.post("/user/ban", authorize([Role.Admin]), this.banUser.bind(this));
-//this.router.post("/user/make_admin", this.makeAdmin.bind(this));
+//this.router.post("/user/make-admin", this.makeAdmin.bind(this));
 
 export async function getAllBatchNumsAdmin() {
     const path = "/admin/batches/all";
@@ -26,7 +26,7 @@ export async function getAllBatchNumsAdmin() {
 }
 
 export async function getAllTasksAdmin(provider: string, batchNum: number) {
-    const path = "/admin/task_queue/all";
+    const path = "/admin/task-queue/all";
     const response = await axios.get(baseUrl + path, { params: { provider, batchNum }, headers });
     const { data } = response;
     console.log(data);
@@ -34,7 +34,7 @@ export async function getAllTasksAdmin(provider: string, batchNum: number) {
 }
 
 export async function getTaskMarkersByBatchNumAdmin(batchNum: number) {
-    const path = "/admin/task_queue/tasks_by_batch_num";
+    const path = "/admin/task-queue/tasks-by-batch-num";
     const response = await axios.get(baseUrl + path, { params: { batchNum }, headers });
     const { data } = response;
     console.log(data);
@@ -42,7 +42,7 @@ export async function getTaskMarkersByBatchNumAdmin(batchNum: number) {
 }
 
 export async function getApartmentsByLocationAdmin(cityName: string) {
-    const path = "/admin/housing/by_location";
+    const path = "/admin/housing/by-location";
     const response = await axios.get(baseUrl + path, { params: { cityName }, headers });
     const { data } = response;
     console.log(data);
@@ -50,15 +50,15 @@ export async function getApartmentsByLocationAdmin(cityName: string) {
 }
 
 export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum: number) {
-    const path = "/admin/housing/by_city_id_and_batch_num";
+    const path = "/admin/housing/by-city-id-and-batch-num";
     const response = await axios.get(baseUrl + path, { params: { cityId, batchNum }, headers });
     const { data } = response;
     console.log(data);
     return data.apartments;
 }
 
-// this.router.get("/housing/by_location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
-// this.router.get("/housing/by_city_id_and_batch_num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
+// this.router.get("/housing/by-location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
+// this.router.get("/housing/by-city-id-and-batch-num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
 
 // export async function getAllHousingAdmin() {
 //     const path = "/admin/housing/all";
@@ -68,14 +68,14 @@ export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum:
 // }
 
 export async function getAllBatchesAdmin() {
-    const path = "/admin/task_queue/all";
+    const path = "/admin/task-queue/all";
     const response = await axios.get(baseUrl + path, { headers });
     const { data } = response;
     return data.tasks;
 }
 
 export async function housingHealthCheck() {
-    const path = "/task_queue/health_check";
+    const path = "/task-queue/health-check";
     console.log(baseUrl + path, "22rm");
     const response = await axios.get(baseUrl + path);
     const { data } = response;
