52af4bac58b42507f1fb528f7757e97b1799c70f
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-01-14 13:39:32 -0800

    convert one part of admin api into hook

--- src/api/adminAPI.ts
+++ src/api/adminAPI.ts
@@ -1,28 +1,54 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-import { adminToken } from "../context/AuthContext";
-
-const baseUrl = process.env.REACT_APP_BACKEND_ADDR;
-
-const headers = {
-    Authorization: `Bearer ${adminToken}`,
-};
+import { useServer } from "../context/ServerContext";
+import { handleError } from "../util/handleError";
 
 // this.router.get("/batches/all", authorize([Role.Admin]), this.getAllBatchNumbers.bind(this));
 //this.router.get("/task-queue/all", authorize([Role.Admin]), this.getAllTasks.bind(this));
 //this.router.get("/housing/by-location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
 // /task-queue/markers-by-batch-num
 //this.router.get("/housing/by-city-id-and-batch-num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
-//// user stuff
+//// user stuff:
 //this.router.post("/user/ban", authorize([Role.Admin]), this.banUser.bind(this));
 //this.router.post("/user/make-admin", this.makeAdmin.bind(this));
 
+export function useGetAllBatchNumsAPI(): { batchNums: number[]; getAllBatchNumsErr: string; loaded: boolean } {
+    const [batchNums, setBatchNums] = useState<number[]>([]);
+    const [getAllBatchNumsErr, setGetAllBatchNumsErr] = useState<string>("");
+    const [loaded, setLoaded] = useState<boolean>(false);
+
+    const server = useServer();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setGetAllBatchNumsErr("");
+                const path = "/admin/batches/all";
+                const response = await server.get(path);
+                const { batchNums } = response.data;
+                setBatchNums(batchNums);
+            } catch (err) {
+                const msg = handleError(err);
+                setGetAllBatchNumsErr(msg);
+            } finally {
+                setLoaded(true);
+            }
+        })();
+    }, []);
+
+    return { batchNums, getAllBatchNumsErr, loaded };
+}
 export async function getAllBatchNumsAdmin() {
     const path = "/admin/batches/all";
     const response = await axios.get(baseUrl + path, { headers });
     const { data } = response;
     console.log(data);
     return data.batchNums;
+}
+
+export function useGetAllTasksAPI() {
+    //
 }
 
 export async function getAllTasksAdmin(provider: string, batchNum: number) {
@@ -33,6 +59,10 @@ export async function getAllTasksAdmin(provider: string, batchNum: number) {
     return data;
 }
 
+export function useGetTaskMarkersByBatchNumAPI() {
+    //
+}
+
 export async function getTaskMarkersByBatchNumAdmin(batchNum: number) {
     const path = "/admin/task-queue/tasks-by-batch-num";
     const response = await axios.get(baseUrl + path, { params: { batchNum }, headers });
@@ -41,12 +71,20 @@ export async function getTaskMarkersByBatchNumAdmin(batchNum: number) {
     return data.tasks;
 }
 
+export function useGetApartmentsByLocationAPI() {
+    //
+}
+
 export async function getApartmentsByLocationAdmin(cityName: string) {
     const path = "/admin/housing/by-location";
     const response = await axios.get(baseUrl + path, { params: { cityName }, headers });
     const { data } = response;
     console.log(data);
     return data.apartments;
+}
+
+export function useGetApartmentsByCityIdAndBatchNumAPI() {
+    //
 }
 
 export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum: number) {
@@ -67,11 +105,19 @@ export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum:
 //     return data.apartments;
 // }
 
+export function useGetAllBatchesAPI() {
+    //
+}
+
 export async function getAllBatchesAdmin() {
     const path = "/admin/task-queue/all";
     const response = await axios.get(baseUrl + path, { headers });
     const { data } = response;
     return data.tasks;
+}
+
+export function healthCheckAPI(where: string) {
+    //
 }
 
 export async function housingHealthCheck() {
