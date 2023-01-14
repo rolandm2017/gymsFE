5d93cfa14274b209374d3c2848a1a865b92b3172
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-01-14 14:00:22 -0800

    more conversion into hooks for admin api

--- src/api/adminAPI.ts
+++ src/api/adminAPI.ts
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 
 import { useServer } from "../context/ServerContext";
 import { handleError } from "../util/handleError";
+import { ITask } from "../interface/Task.interface";
+import { GetTaskMarkersByBatchNum } from "../interface/payload/GetTaskMarkersByBatchNum.interface";
+import { IHousing } from "../interface/Housing.interface";
+import { GetHousingByLocation } from "../interface/payload/GetHousingByLocation.interface";
+import { GetHousingByCityIdAndBatchNum } from "../interface/payload/GetHousingByCityIdAndBatchNum.interface";
 
 // this.router.get("/batches/all", authorize([Role.Admin]), this.getAllBatchNumbers.bind(this));
 //this.router.get("/task-queue/all", authorize([Role.Admin]), this.getAllTasks.bind(this));
@@ -39,61 +44,163 @@ export function useGetAllBatchNumsAPI(): { batchNums: number[]; getAllBatchNumsE
 
     return { batchNums, getAllBatchNumsErr, loaded };
 }
-export async function getAllBatchNumsAdmin() {
-    const path = "/admin/batches/all";
-    const response = await axios.get(baseUrl + path, { headers });
-    const { data } = response;
-    console.log(data);
-    return data.batchNums;
+
+export function useGetAllTasksAPI(): { allTasks: ITask[]; getAllTasksErr: string; loaded: boolean } {
+    const [allTasks, setAllTasks] = useState<ITask[]>([]);
+    const [getAllTasksErr, setGetAllTasksErr] = useState("");
+    const [loaded, setLoaded] = useState<boolean>(false);
+
+    const server = useServer();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setGetAllTasksErr("");
+                const path = "/admin/batches/all";
+                const response = await server.get(path);
+                const tasks = response.data;
+                setAllTasks(tasks);
+            } catch (err) {
+                const msg = handleError(err);
+                setGetAllTasksErr(msg);
+            } finally {
+                setLoaded(true);
+            }
+        })();
+    }, []);
+
+    return { allTasks, getAllTasksErr, loaded };
 }
 
-export function useGetAllTasksAPI() {
-    //
+export function useGetTaskMarkersByBatchNumAPI(): {
+    taskMarkersForBatchNum: ITask[];
+    runGetTaskMarkersByBatchNum: Function;
+    getTaskMarkerByBatchNumErr: string;
+    loaded: boolean;
+} {
+    const [taskMarkersForBatchNum, setTaskMarkersForBatchNum] = useState<ITask[]>([]);
+    const [getTaskMarkerByBatchNumErr, setGetTaskMarkerByBatchNumErr] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [payload, setPayload] = useState<GetTaskMarkersByBatchNum | undefined>(undefined);
+
+    const server = useServer();
+
+    function runGetTaskMarkersByBatchNum(batchNum: number) {
+        setPayload({ batchNum });
+        setLoaded(false);
+    }
+
+    useEffect(() => {
+        if (payload && !loaded) {
+            (async () => {
+                try {
+                    setGetTaskMarkerByBatchNumErr("");
+                    const path = "/admin/batches/all";
+                    const response = await server.get(path);
+                    const tasks = response.data;
+                    setTaskMarkersForBatchNum(tasks);
+                } catch (err) {
+                    const msg = handleError(err);
+                    setGetTaskMarkerByBatchNumErr(msg);
+                } finally {
+                    setLoaded(true);
+                    setPayload(undefined);
+                }
+            })();
+        }
+    }, [payload]);
+
+    return { taskMarkersForBatchNum, runGetTaskMarkersByBatchNum, getTaskMarkerByBatchNumErr, loaded };
 }
 
-export async function getAllTasksAdmin(provider: string, batchNum: number) {
-    const path = "/admin/task-queue/all";
-    const response = await axios.get(baseUrl + path, { params: { provider, batchNum }, headers });
-    const { data } = response;
-    console.log(data);
-    return data;
+export function useGetHousingByLocationAPI(): {
+    housingByLocation: IHousing[];
+    runGetHousingByLocation: Function;
+    getHousingByLocationErr: string;
+    loaded: boolean;
+} {
+    const [housingByLocation, setHousingByLocation] = useState<IHousing[]>([]);
+    const [getHousingByLocationErr, setGetHousingByLocationErr] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [payload, setPayload] = useState<GetHousingByLocation | undefined>(undefined);
+
+    const server = useServer();
+
+    function runGetHousingByLocation(cityName: string) {
+        setPayload({ cityName });
+        setLoaded(false);
+    }
+
+    useEffect(() => {
+        if (payload && !loaded) {
+            (async () => {
+                try {
+                    setGetHousingByLocationErr("");
+                    const path = "/admin/batches/all";
+                    const response = await server.get(path);
+                    const tasks = response.data;
+                    setHousingByLocation(tasks);
+                } catch (err) {
+                    const msg = handleError(err);
+                    setGetHousingByLocationErr(msg);
+                } finally {
+                    setLoaded(true);
+                    setPayload(undefined);
+                }
+            })();
+        }
+    }, [payload]);
+
+    return { housingByLocation, runGetHousingByLocation, getHousingByLocationErr, loaded };
 }
 
-export function useGetTaskMarkersByBatchNumAPI() {
-    //
+export function useGetHousingByCityIdAndBatchNumAPI(): {
+    housingByCityIdAndBatchNum: IHousing[];
+    runGetHousingByCityIdAndBatchNum: Function;
+    getHousingByCityIdAndBatchNumErr: string;
+    loaded: boolean;
+} {
+    const [housingByCityIdAndBatchNum, setHousingByCityIdAndBatchNum] = useState<IHousing[]>([]);
+    const [getHousingByCityIdAndBatchNumErr, setGetHousingByCityIdAndBatchNumErr] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [payload, setPayload] = useState<GetHousingByCityIdAndBatchNum | undefined>(undefined);
+
+    const server = useServer();
+
+    function runGetHousingByCityIdAndBatchNum(cityId: number, batchNum: number) {
+        setPayload({ cityId, batchNum });
+        setLoaded(false);
+    }
+
+    useEffect(() => {
+        if (payload && !loaded) {
+            (async () => {
+                try {
+                    setGetHousingByCityIdAndBatchNumErr("");
+                    const path = "/admin/housing/by-city-id-and-batch-num";
+                    const response = await server.get(path, { params: { payload } });
+                    const tasks = response.data;
+                    setHousingByCityIdAndBatchNum(tasks);
+                } catch (err) {
+                    const msg = handleError(err);
+                    setGetHousingByCityIdAndBatchNumErr(msg);
+                } finally {
+                    setLoaded(true);
+                    setPayload(undefined);
+                }
+            })();
+        }
+    }, [payload]);
+
+    return { housingByCityIdAndBatchNum, runGetHousingByCityIdAndBatchNum, getHousingByCityIdAndBatchNumErr, loaded };
 }
 
-export async function getTaskMarkersByBatchNumAdmin(batchNum: number) {
-    const path = "/admin/task-queue/tasks-by-batch-num";
-    const response = await axios.get(baseUrl + path, { params: { batchNum }, headers });
-    const { data } = response;
-    console.log(data);
-    return data.tasks;
-}
-
-export function useGetApartmentsByLocationAPI() {
-    //
-}
-
-export async function getApartmentsByLocationAdmin(cityName: string) {
-    const path = "/admin/housing/by-location";
-    const response = await axios.get(baseUrl + path, { params: { cityName }, headers });
-    const { data } = response;
-    console.log(data);
-    return data.apartments;
-}
-
-export function useGetApartmentsByCityIdAndBatchNumAPI() {
-    //
-}
-
-export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum: number) {
-    const path = "/admin/housing/by-city-id-and-batch-num";
-    const response = await axios.get(baseUrl + path, { params: { cityId, batchNum }, headers });
-    const { data } = response;
-    console.log(data);
-    return data.apartments;
-}
+// export async function getApartmentsByCityIdAndBatchNum(cityId: number, batchNum: number) {
+//     const response = await axios.get(baseUrl + path, { params: { cityId, batchNum }, headers });
+//     const { data } = response;
+//     console.log(data);
+//     return data.apartments;
+// }
 
 // this.router.get("/housing/by-location", authorize([Role.Admin]), this.getApartmentsByLocation.bind(this));
 // this.router.get("/housing/by-city-id-and-batch-num", authorize([Role.Admin]), this.getApartmentsByCityIdAndBatchNum.bind(this));
---
+++ src/interface/payload/GetHousingByCityIdAndBatchNum.interface.ts
@@ -0,0 +1,4 @@
+export interface GetHousingByCityIdAndBatchNum {
+    cityId: number;
+    batchNum: number;
+}
