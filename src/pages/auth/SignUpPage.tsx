b72b9e7a10a03042229d6563435c04be27c3d291
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-01-17 16:57:18 -0800

    signup pg looks done on mobile

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -84,12 +84,11 @@ const SignUpPage: React.FC<{}> = () => {
 
     return (
         <main className="h-full w-full">
-            <div className="h-1/2 absolute z-20 bg-red-500"></div>
-            <div className="h-full flex">
-                <div className="h-full w-1/2 shadow-2xl z-20 primaryBlueBg">
+            <div className="h-full flex flex-col sm:flex-row">
+                <div className="h-full w-1/2 shadow-2xl z-20 hidden sm:block primaryBlueBg">
                     {/* // left hand side - shows on desktop, hidden on mobile */}
                     <div className="w-full z-50 border-2 border-red-400 flex justify-end">
-                        <img src={SignUpPageCurve} />
+                        <img src={SignUpPageCurve} alt="bottom left quadrant of a full circle" />
                         {/* // todo (1) - find soruce of horizontal scroll
                         // todo 1.5 - make two separate containers, left and top, for desktop and mobile code
                         // todo (2) - mobile design */}
@@ -101,16 +100,27 @@ const SignUpPage: React.FC<{}> = () => {
                         thin={true}
                     />
                 </div>
-                <div className="absolute z-40 h-screen w-screen border-red-400">
-                    {/* // middle person */}
-                    <div className="h-full w-full flex justify-center absolute bottom-0 z-40  border-black">
-                        <img src={SignUpMan} alt="sign up man" className="pl-20 pt-36 signupManBgImg" />
+                <div className="h-min w-full shadow-2xl z-20 relative block sm:hidden primaryBlueBg">
+                    {/* // top - shows on mobile, hidden on desktop */}
+                    <div className="w-full max-h-96 z-50 border-2 border-red-400 flex justify-end">
+                        <img src={SignUpPageCurve} alt="bottom left quadrant of a full circle" />
+                    </div>
+                    <div className="h-1/2 mb-8 ml-8 text-left z-50 absolute bottom-0 block sm:hidden">
+                        <div className="h-full w-full flex items-end">
+                            <p className="text-4xl font-medium">Sign up</p>
+                        </div>
                     </div>
                 </div>
-                <div className="h-full w-1/2 bg-white flex justify-center items-center">
+                <div className="absolute z-40 h-screen w-screen hidden sm:flex border-red-400">
+                    {/* // middle person */}
+                    <div className="h-full w-full flex justify-center absolute bottom-0 z-40  border-black">
+                        <img src={SignUpMan} alt="sign up man" className="pl-0 md:pl-20 pt-36 signupManBgImg" />
+                    </div>
+                </div>
+                <div className="h-full w-full sm:w-1/2 bg-white flex justify-center items-start sm:items-center">
                     {/* // right hand side */}
-                    <div className="w-1/2">
-                        <div className="mb-8 text-left">
+                    <div className="w-1/2 mt-12 sm:mt-0">
+                        <div className="mb-8 text-left hidden sm:block">
                             <p className="text-4xl font-medium">Sign up</p>
                         </div>
                         <div>
