40e220adeb3b849c1431047075b43df1a166e5ce
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-01-31 07:23:47 -0800

    modify pagination on details bar container

--- src/components/detailsBar/DetailsBarContainer.tsx
+++ src/components/detailsBar/DetailsBarContainer.tsx
@@ -13,10 +13,14 @@ const DetailsBarContainer: React.FC<DetailsBarContainerProps> = ({ apartments, c
     const [currentlyDisplayed, setCurrentlyDisplayed] = useState<IHousing[]>([]);
 
     useEffect(() => {
-        const endOfPg = (currentPage + 1) * 15;
-        const current = apartments.slice(currentPage * 15, endOfPg);
+        console.log(currentPage, "16rm");
+        const startOfPg = (currentPage - 1) * 15;
+        const endOfPg = currentPage * 15;
+        const current = apartments.slice(startOfPg, endOfPg);
+        console.log(apartments, current, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 18rm");
         setCurrentlyDisplayed(current);
-    }, [currentPage]);
+    }, [apartments, currentPage]);
+    console.log(currentlyDisplayed, "20rm");
 
     return (
         <div>
