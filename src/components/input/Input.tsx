aabbd4aea2c72800bb75d8a6eff59854749a94ae
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-02-04 17:34:06 -0800

    update verify account page aesthetics

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -12,7 +12,7 @@ const Input: React.FC<InputProps> = ({ type, placeholder, changeReporter }) => {
     return (
         <div className="mr-1">
             <input
-                className={`inputText  ${type === "text" ? "w-48 addrIcon" : "w-40 distanceIcon"}`}
+                className={`inputText pl-2 ${type === "text" ? "w-48 addrIcon" : "w-40 distanceIcon"}`}
                 type={type}
                 placeholder={placeholder}
                 onChange={e => {
--- src/pages/auth/VerifyAccountPage.tsx
+++ src/pages/auth/VerifyAccountPage.tsx
@@ -9,21 +9,25 @@ const VerifyAccountPage: React.FC<{}> = () => {
     const { success, err, runSendVerificationCode } = useSendVerificationCodeAPI();
     return (
         <div className="h-full w-full flex justify-center items-center">
-            <div className="w-1/3 h-1/3 flex flex-col">
+            <div className="w-1/3 h-1/3 flex flex-col items-center">
                 <div>
                     <p>Thanks for joining </p>
                     <p>Apartments Near Gyms.</p>
                 </div>
-                <div>
-                    <h3>Input your verification code</h3>
-                    <Input type="text" placeholder="Your verification code" changeReporter={setInputtedCode} />
-                    <Button
-                        text="Submit"
-                        type="Opaque"
-                        onClickHandler={() => {
-                            runSendVerificationCode(inputtedCode);
-                        }}
-                    />
+                <div className="mt-6 w-96 flex flex-col items-center">
+                    <h3 className="py-3">Input your verification code</h3>
+                    <div className="mb-4 w-60">
+                        <Input type="text" placeholder="Your verification code" changeReporter={setInputtedCode} />
+                    </div>
+                    <div>
+                        <Button
+                            text="Submit"
+                            type="Opaque"
+                            onClickHandler={() => {
+                                runSendVerificationCode(inputtedCode);
+                            }}
+                        />
+                    </div>
                 </div>
                 {success ? (
                     <p>
